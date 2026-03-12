bf34a2b274477031b09a024fcbf8957d1576ebfe
lucia117 <luciascharff@gmail.com>
2026-03-12 07:35:18 -0300

    refactor: Reorder SQL statements in migration to drop triggers and functions before tables

--- migrations/0005_create_edge_devices_tables.down.sql
+++ migrations/0005_create_edge_devices_tables.down.sql
@@ -1,9 +1,9 @@
--- Drop device_events table
-DROP TABLE IF EXISTS device_events;
-
--- Drop edge_devices table
-DROP TABLE IF EXISTS edge_devices;
-
--- Drop trigger and function
+-- Drop trigger and function first (before tables)
 DROP TRIGGER IF EXISTS trg_edge_devices_updated_at ON edge_devices;
 DROP FUNCTION IF EXISTS update_edge_devices_updated_at();
+
+-- Drop device_events table (references edge_devices via foreign key)
+DROP TABLE IF EXISTS device_events;
+
+-- Drop edge_devices table last
+DROP TABLE IF EXISTS edge_devices;
